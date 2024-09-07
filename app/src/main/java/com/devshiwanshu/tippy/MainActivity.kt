f6767f18c06e50cfb2ce728a8f70672ed02ae216
Shiwanshu Shubham <shiwanshudev@protonmail.com>
2024-09-07 19:00:00 +0530

    Basics of jetpack compose

--- app/src/main/java/com/devshiwanshu/tippy/MainActivity.kt
+++ app/src/main/java/com/devshiwanshu/tippy/MainActivity.kt
@@ -3,45 +3,197 @@ package com.devshiwanshu.tippy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.draw.clip
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Surface
 import com.devshiwanshu.tippy.ui.theme.TippyTheme
+import androidx.compose.foundation.border
+import androidx.compose.material3.MaterialTheme
+import android.content.res.Configuration
+import android.provider.Telephony.Sms.Conversations
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.clickable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+
+
+/**
+ * SampleData for Jetpack Compose Tutorial
+ */
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "Lexi",
+            "Test...Test...Test..."
+        ),
+        Message(
+            "Lexi",
+            """List of Android versions:
+            |Android KitKat (API 19)
+            |Android Lollipop (API 21)
+            |Android Marshmallow (API 23)
+            |Android Nougat (API 24)
+            |Android Oreo (API 26)
+            |Android Pie (API 28)
+            |Android 10 (API 29)
+            |Android 11 (API 30)
+            |Android 12 (API 31)""".trim()
+        ),
+        Message(
+            "Lexi",
+            """I think Kotlin is my favorite programming language.
+            |It's so much fun!""".trim()
+        ),
+        Message(
+            "Lexi",
+            "Searching for alternatives to XML layouts..."
+        ),
+        Message(
+            "Lexi",
+            """Hey, take a look at Jetpack Compose, it's great!
+            |It's the Android's modern toolkit for building native UI.
+            |It simplifies and accelerates UI development on Android.
+            |Less code, powerful tools, and intuitive Kotlin APIs :)""".trim()
+        ),
+        Message(
+            "Lexi",
+            "It's available from API 21+ :)"
+        ),
+        Message(
+            "Lexi",
+            "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
+        ),
+        Message(
+            "Lexi",
+            "Android Studio next version's name is Arctic Fox"
+        ),
+        Message(
+            "Lexi",
+            "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
+        ),
+        Message(
+            "Lexi",
+            "I didn't know you can now run the emulator directly from Android Studio"
+        ),
+        Message(
+            "Lexi",
+            "Compose Previews are great to check quickly how a composable layout looks like"
+        ),
+        Message(
+            "Lexi",
+            "Previews are also interactive after enabling the experimental setting"
+        ),
+        Message(
+            "Lexi",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+    )
+}
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            TippyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContent{
+            TippyTheme() {
+                Conversation(SampleData.conversationSample)
+        } }
+    }
+}
+
+data class Message(val author:String, val body:String)
+
+@Composable
+fun MessageCard(msg: Message){
+   Row(modifier=Modifier.padding(8.dp)){
+       Image(
+           painter = painterResource(R.drawable.profile_picture),
+           contentDescription = "Contact profile picture",
+           modifier=Modifier.clip(CircleShape).size(40.dp).border(1.5.dp, MaterialTheme.colorScheme.primary)
+       )
+       Spacer(modifier=Modifier.width(8.dp))
+
+       // We keep track if the message is expanded or not in this
+       // variable
+       var isExpanded by remember { mutableStateOf(false) }
+       // surfaceColor will be updated gradually from one color to the other
+       val surfaceColor by animateColorAsState(
+           if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+       )
+       Column(modifier = Modifier.clickable { isExpanded = !isExpanded }){Column{
+           Text(msg.author, color = MaterialTheme.colorScheme.secondary)
+           Spacer(modifier=Modifier.height(4.dp))
+           Surface(
+               shape = MaterialTheme.shapes.medium,
+               shadowElevation = 1.dp,
+               // surfaceColor color will be changing gradually from primary to surface
+               color = surfaceColor,
+               // animateContentSize will change the Surface size gradually
+               modifier = Modifier.animateContentSize().padding(1.dp)
+           ) {
+               Text(
+                   text = msg.body,
+                   modifier = Modifier.padding(all = 4.dp),
+                   // If the message is expanded, we display all its content
+                   // otherwise we only display the first line
+                   maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                   style = MaterialTheme.typography.bodyMedium
+               )
+           }
+       }}
+
+   }
+}
+
+@Preview(name = "Light Mode")
+@Preview(
+    name = "Dark Mode",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+@Composable
+fun MessageCardPreview(){
+    TippyTheme {
+        Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+        MessageCard(msg=Message("Lexy", "Look at Jetpack compose it is okay!"))
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Conversation(messages: List<Message>){
+    LazyColumn {
+        items(messages){
+            message->
+            MessageCard(message)
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
+fun ConversationPreview(){
     TippyTheme {
-        Greeting("Android")
+        Conversation(SampleData.conversationSample)
     }
 }
